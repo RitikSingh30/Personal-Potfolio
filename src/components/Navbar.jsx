d51b7d2fa18df8c3e780083bbb36028402e3f86f
Ritik Singh <ritiksingh@Ritiks-MacBook-Air.local>
2024-07-12 15:11:15 +0530

    fixed the bug in the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,6 +26,8 @@ const Navbar = () => {
   }
   
   const controllNavbar = () => {
+    var width = window.innerWidth
+    if (width > 768) return
     const curState = showNavbarData ;
     setShowNavbarData(!curState);
   }
@@ -34,7 +36,7 @@ const Navbar = () => {
     <header className={`sticky md:top-0 z-10 ${darkMode ? "bg-DarkColor text-white" : "bg-white text-black"} mmd:fixed mmd:bottom-0
     mmd:w-[100%]`}>
       <nav className='md:max-w-[715px] lg:max-w-[960px] mmd:max-w-[960px] mx-auto flex md:justify-between py-[1.5rem] mmd:py-[0.6rem] mmd:px-6'>
-        <a href='#' className={`${showNavbarData ? 'block' : 'hidden'} font-poppins font-poppins-500 mmd:text-[1.2rem] text-[1rem] hover:text-firstColor`}>Ritik</a>
+        <a href='#' className={`${showNavbarData ? 'block' : 'hidden'} md:block font-poppins font-poppins-500 mmd:text-[1.2rem] text-[1rem] hover:text-firstColor`}>Ritik</a>
         <div className='flex gap-x-3 mmd:flex-col mmd:w-full mmd:gap-y-3'>
           <Scrollspy items={["Home","About","Skills","Qualification","Projects","Contact Me"]} currentClassName="text-firstColor" 
           className={`flex gap-x-8 font-poppins font-poppins-500 text-[0.875rem] mmd:grid-rows-2 mmd:grid-cols-3 
@@ -67,7 +69,7 @@ const Navbar = () => {
               }
               </div>
             }
-            <div onClick={changePageModeColor} className={`cursor-pointer hover:text-firstColor ${showNavbarData ? 'block' : 'hidden'}`}>
+            <div onClick={changePageModeColor} className={`cursor-pointer hover:text-firstColor ${showNavbarData ? 'block' : 'hidden'} md:block`}>
               {
                 darkMode ? (<BiSun className='text-[1.1rem] mmd:text-[1.2rem]'/>) : (<BiMoon className='text-[1.1rem] mmd:text-[1.4rem]'/>)
               }
